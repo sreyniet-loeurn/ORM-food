41a57833d36790751a7dcbda14290c2a21d0ae1a
sreyroth <osreyroth.student@puthisastra.edu.kh>
2022-04-30 15:56:39 +0700

    change some style

--- footer.php
+++ footer.php
@@ -1,33 +1,57 @@
-<div class="container-fluid animatedParent" style="background:#F8F9FA; padding:40px;"> 
-		    <div class="container">
-		      
-			      <div class="row animated fadeIn">
-				      
-				  <div class="col-sm-4" style=" border-right:1px solid black;">
-					    <ul>
-                          <li><h5>CUSTOMER SECTION</h5></li>
-						  <li><a href="form/index.php">Customer Login and Register</a></li>
-						 
-						</ul>
-				</div>
-				<div class="col-sm-4" style=" border-right:1px solid black;">
-					    <ul>
-                          <li><h5>ADMIN SECTION</h5></li>
-						  <li><a href="admin.php">Admin Login</a></li>
-						  
-						</ul>
-					</div>
-					
-					  <div class="col-sm-4" style=" border-right:1px solid black;">
-					     <ul>
-						  <li><h5>HOTEL LINKS</h5></li>
-						  <li><a href="vendor-new.php">Register On Food Hunt</a></li>
-						  <li><a href="vendor_login.php">Hotel Account Login</a></li>
-						  <li><a href="food.php">Add Foods</a></li>
-						  
-						</ul>
-					  </div>
-				  </div>
-			 </div>
-			 			 
-		  </div>
+<footer class="flex-rw">
+
+	<ul class="footer-list-top">
+		<li>
+			<h4 class="footer-list-header textstyle">About Pavilion</h4>
+		</li>
+		<li><a href='/promos.html' class="generic-anchor textstyle" itemprop="significantLink">PROMOS</a></li>
+		<li><a href='/retailers/new-retailers.html' class="generic-anchor textstyle" itemprop="significantLink">BECOME A RETAILER</a></li>
+
+		<li><a href='/job-openings.html' itemprop="significantLink" class="generic-anchor textstyle">JOB OPENINGS</a></li>
+
+		<li><a href='/shop/about-show-schedule' class="generic-anchor textstyle" itemprop="significantLink">EVENTS</a></li>
+	</ul>
+	<ul class="footer-list-top">
+		<li>
+			<h4 class="footer-list-header textstyle">The Gift Selection</h4>
+		</li>
+
+
+		<li><a href='/Home-Decor/cat/id/64' class="generic-anchor textstyle">HOME DECOR</a></li>
+		<li><a href='/Mugs/cat/id/32' class="generic-anchor textstyle">MUGS</a></li>
+		<li><a href='/Pet-Lover/cat/id/108' class="generic-anchor textstyle">PET LOVER</a></li>
+		<li><a href='/Ladies-Accessories/cat/id/117' class="generic-anchor textstyle" target="_blank">HANDBAGS & JEWELRY</a></li>
+	</ul>
+	<ul class="footer-list-top">
+		<li id='help'>
+			<h4 class="footer-list-header textstyle">Please Help Me</h4>
+		</li>
+		<li><a href='/faq.html' class="generic-anchor textstyle" itemprop="significantLink">FAQ</a></li>
+		<li id='find-a-store'><a href='/shop/store-locator' class="generic-anchor textstyle" itemprop="significantLink">STORE LOCATOR</a></li>
+		<li id='user-registration'><a href='/shop/user-registration?URL=' class="generic-anchor textstyle" itemprop="significantLink">NEW USERS</a></li>
+		<li id='order-tracking'><a href='/shop/order-status' itemprop="significantLink" class="generic-anchor textstyle">ORDER STATUS</a></li>
+	</ul>
+	<section class="footer-social-section flex-rw">
+		<span class="footer-social-overlap textstyle footer-social-connect">
+			CONNECT <span class="footer-social-small">with</span> US
+		</span>
+		<span class="footer-social-overlap footer-social-icons-wrapper">
+			<a href="https://www.pinterest.com/paviliongift/" class="generic-anchor" target="_blank" title="Pinterest" itemprop="significantLink"><i class="fab fa-pinterest" aria-hidden="true"></i></a>
+			<a href="https://www.facebook.com/paviliongift" class="generic-anchor" target="_blank" title="Facebook" itemprop="significantLink"><i class="fab fa-facebook"></i></a>
+			<a href="https://twitter.com/PavilionGiftCo" class="generic-anchor" target="_blank" title="Twitter" itemprop="significantLink"><i class="fab fa-twitter"></i></a>
+			<a href="http://instagram.com/paviliongiftcompany" class="generic-anchor" target="_blank" title="Instagram" itemprop="significantLink"><i class="fab fa-instagram"></i></a>
+			<a href="https://www.youtube.com/channel/UCYgUODvd0qXbu_LkUWpTVEg" class="generic-anchor" target="_blank" title="Youtube" itemprop="significantLink"><i class="fab fa-youtube"></i></a>
+			<a href="https://plus.google.com/+Paviliongift/posts" class="generic-anchor" target="_blank" title="Google Plus" itemprop="significantLink"><i class="fab fa-google-plus"></i></a>
+		</span>
+	</section>
+	<section class="footer-bottom-section flex-rw">
+		<div class="footer-bottom-wrapper textstyle">
+			<i class="fa fa-copyright" role="copyright">
+
+			</i> 2019 Pavilion in <address class="footer-address" role="company address">Bergen, NY</address><span class="footer-bottom-rights"> - All Rights Reserved - </span>
+		</div>
+		<div class="footer-bottom-wrapper">
+			<a href="/terms-of-use.html" class="generic-anchor textstyle" rel="nofollow">Terms</a> | <a href="/privacy-policy.html" class="generic-anchor textstyle" rel="nofollow">Privacy</a>
+		</div>
+	</section>
+</footer>
